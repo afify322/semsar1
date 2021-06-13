f0c686a7e1da86a85924653c770383f9b0893621
afify322 <76813264+afify322@users.noreply.github.com>
2021-06-13 14:14:37 +0200

    Update update-transaction.dto.ts

--- src/transactions/dto/update-transaction.dto.ts
+++ src/transactions/dto/update-transaction.dto.ts
@@ -2,9 +2,9 @@ import { PartialType } from '@nestjs/mapped-types';
 import { IsNotEmpty, Length } from 'class-validator';
 import { CreateTransactionDto } from './create-transaction.dto';
 
-export class UpdateTransactionDto extends PartialType(CreateTransactionDto) {
+export class UpdateTransactionDto {
   @IsNotEmpty({ message: 'يجب ادخال مبلغ البيع' })
-  @Length(1,5,{message:'اقصى مبلغ يمكن ادخاله هو 99,999'})
+
   sellCash: number;
   @IsNotEmpty({ message: 'يجب ادخال تاريخ البيع' })
   sellDate: Date;
@@ -12,6 +12,6 @@ export class UpdateTransactionDto extends PartialType(CreateTransactionDto) {
   @IsNotEmpty({ message: 'يجب ادخال اسم التاجر' })
   dealerName: string;
   @IsNotEmpty({ message: 'يجب ادخال رقم التليفون ' })
-  @Length(9,11,{message:'رقم التليفون غير صحيح'})
+  @Length(8,11,{message:'رقم التليفون غير صحيح'})
   phoneNumber: string;
 }
